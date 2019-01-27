125f44dd62eefe95e25d261be2c372780cd15065
Joep Kockelkorn <joep.kockelkorn@qnh.nl>
2019-01-27 19:48:19 +0100

    do not use viewencapsulation (to allow custom class to be used)

--- projects/ngx-form-errors/src/lib/components/control-error/control-error.component.ts
+++ projects/ngx-form-errors/src/lib/components/control-error/control-error.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Inject, Input, OnDestroy, TemplateRef } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, ViewEncapsulation } from '@angular/core';
 
 import { FORM_ERRORS_CONFIG } from '../../injection-tokens';
 import { FormErrorsConfig } from '../../models/form-errors-config.model';
@@ -8,7 +8,8 @@ import { ControlErrorBaseComponent } from './control-error-base-component';
 @Component({
   selector: 'nfe-control-error',
   templateUrl: './control-error.component.html',
-  styleUrls: ['./control-error.component.scss']
+  styleUrls: ['./control-error.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ControlErrorComponent extends ControlErrorBaseComponent {
   constructor(@Inject(FORM_ERRORS_CONFIG) public config: FormErrorsConfig, cdr: ChangeDetectorRef) {
